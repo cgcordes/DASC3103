dda07aa9f2d0350a0050360e7906db08eb983cd5
cgcordes <111714762+cgcordes@users.noreply.github.com>
2023-10-01 16:37:42 -0500

    Add files via upload

--- credit_card_reduce.py
+++ credit_card_reduce.py
@@ -1,25 +1,13 @@
 #!/usr/bin/env python 3
 
 import sys
-
 for line in sys.stdin:
     (key, value) = line.strip().split("\t")
-
-    value = int(value)  # Assuming the value is an integer
-
-
+    value = int(value) 
     if last_key and last_key != key:
-
         print("%s\t%s" % (last_key, total_sum))
-
         (last_key, total_sum) = (key, value)
-
     else:
-
-        (last_key, total_sum) = (key, total_sum + value)
-
- 
-
+        (last_key, total_sum) = (key, total_sum + value)    
 if last_key:
-
     print("%s\t%s" % (last_key, total_sum))
