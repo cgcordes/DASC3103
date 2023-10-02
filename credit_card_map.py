61c04a3fda92b668a517ba1ab510199a142c52e1
cgcordes <111714762+cgcordes@users.noreply.github.com>
2023-10-02 09:35:07 -0500

    Add files via upload

--- credit_card_map.py
+++ credit_card_map.py
@@ -3,7 +3,6 @@
 import sys
 for index, row in sys.stdin.iterrows():
     ID = row[0]
-    y = row[24]
     for i in range(6, 12):
         value_at_xi = row[i]
         output = "%s\t%s" % (ID, value_at_xi)
