bcdd7f07fd735be394e33eaf843c6d43083e4720
cgcordes <111714762+cgcordes@users.noreply.github.com>
2023-10-02 14:05:38 -0500

    Add files via upload

--- credit_card_reduce_trial_2.py
+++ credit_card_reduce_trial_2.py
@@ -2,14 +2,21 @@
 
 import sys
 
-(last_key, total_sum, count) = (None, 0, 0)
+data = []
+(last_key, total_sum) = (None, 0)
 for line in sys.stdin:
     (key, value) = line.strip().split("\t")
-    value = int(value) 
+    value = int(value)
     if last_key and last_key != key:
-        print("%s\t%s\t%s" % (last_key, total_sum, count))
-        (last_key, total_sum, count) = (key, value, 1)
+        data.append((last_key, total_sum))
+        (last_key, total_sum) = (key, value)
     else:
-        (last_key, total_sum, count) = (key, total_sum + value, count + 1)    
+        (last_key, total_sum) = (key, total_sum + value)
 if last_key:
-    print("%s\t%s\t%s" % (last_key, total_sum, count))
+    data.append((last_key, total_sum))
+
+data.sort(key=lambda x: x[1], reverse=True)
+
+# Print the top 50 highest total_sum values.
+for i, (key, total_sum) in enumerate(data[:50], 1):
+    print(f"{i}. {key}\t{total_sum}")
